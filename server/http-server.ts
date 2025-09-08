50779339513d9ab9734a7801b1b2c712b3bd5131
Abdessamad Abba <a.abba@svg.de>
2025-09-09 00:45:38 +0200

    Increase server payload limits to 50MB for image uploads

--- server/http-server.ts
+++ server/http-server.ts
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 // Add offline API routes
 app.use('/api/offline', offlineApiRouter);
