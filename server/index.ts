046518364cb0eb2e72550c8c9d47a64e6c77129c
Abdessamad Abba <a.abba@svg.de>
2025-11-13 22:58:48 +0100

    Increase Express body parser limit to 50MB for product images
    FIX: PayloadTooLargeError when uploading product images
    
    PROBLEM:
    - Users got 'request entity too large' error when adding images to products
    - Default Express body parser limit was 100KB
    - Product images (especially base64 encoded) can be 2-3MB
    
    SOLUTION:
    - Increased body parser limit to 50MB in server/index.ts
    - Allows product images and other large payloads
    - Matches limit already set in http-server.ts
    
    CHANGES:
    - express.json({ limit: '50mb' })
    - express.urlencoded({ extended: false, limit: '50mb' })
    
    Now users can upload product images without payload size errors!

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,8 @@ import { setupVite, serveStatic } from "./vite";
 import { offlineApiRouter } from "./offline-api";
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 app.use((req, res, next) => {
   const start = Date.now();
